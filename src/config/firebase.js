42f311d730a57e416b38e187b0a32dd2c85cc750
Ahmed Jamal <81833844+AhhmedJamal@users.noreply.github.com>
2024-01-12 22:48:06 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,5 +13,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore(app,{experimentalAutoDetectLongPolling: true,});
 export const auth = getAuth(app);
