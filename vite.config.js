ae16293981c217ef764c60b6101f184be7b4c475
AhhmedJamal <ahhmed.jamal@gmail.com>
2024-06-02 14:09:38 +0300

    config firebase update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { config } from "dotenv";
-
-config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
